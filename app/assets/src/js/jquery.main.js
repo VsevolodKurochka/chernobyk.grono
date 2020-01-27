489c3b86f057fb9af223ff505f8b9b55a89c86b5
VsevolodKurochka <seva.kurochka@gmail.com>
2020-01-27 20:13:51 +0200

    create modal video

--- app/assets/src/js/jquery.main.js
+++ app/assets/src/js/jquery.main.js
@@ -106,6 +106,12 @@ $(document).ready(function(){
 		$('body').addClass('modal-open');
 	}
 
+	function removeSrcFromModalVideo(modal) {
+		if (modal.find('.modal__iframe')) {
+			modal.find('.modal__iframe').removeAttr('src');
+		}
+	}
+
 	$('[data-action="modal"]').click(function (e) {
 		e.preventDefault();
 		var modal = $(this).attr('data-open');
@@ -114,14 +120,24 @@ $(document).ready(function(){
 
 	$('[data-close="modal"]').click(function (e) {
 		e.preventDefault();
+		var modal = $(this).closest('.modal');
 		$(this).closest('.modal').removeClass('modal_showing_in');
 		$('body').removeClass('modal-open');
+		removeSrcFromModalVideo(modal);
+	});
+
+	$('[data-video]').on('click', function () {
+		var video = $(this).attr('data-video');
+		var modal = $(this).attr('data-open');
+		$(modal).find('.modal__iframe').attr('src', video);
 	});
 
 	$('body').on('click', function(e) {
 		if ($(e.target).hasClass('modal')) {
-			var modal = $(e.target).attr('id');
-			$('#' + modal).removeClass('modal_showing_in');
+			var modalId = $(e.target).attr('id');
+			var modal = $('#' + modalId);
+			modal.removeClass('modal_showing_in');
+			removeSrcFromModalVideo(modal);
 			$('body').removeClass('modal-open');
 		}
 	});
@@ -181,7 +197,13 @@ $(document).ready(function(){
 		});
 		setTimeout(function () {
 			$('.preloader').hide();
-		}, 1500);
+		}, 4500);
+
+		$("#js-preloader-video").on("loadstart", function() {
+			setTimeout(function () {
+				$('#js-preloader-video').hide();
+			}, 3000);
+		});
 	});
 	var delimiter = $(window).width() > 768 ? 5 : 15;
 	$(window).on('scroll', function() {
