81cef3ef9f0b649cc97b027ca2c05d28e4827a0b
VsevolodKurochka <seva.kurochka@gmail.com>
2020-04-03 12:06:50 +0300

    create page webinar video 7

---
+++ app/webinar-video-7.php
@@ -0,0 +1,7 @@
+<?php
+  require_once 'connect.php';
+  echo $twig->render('webinar-video.twig', [
+      'videoTitle' => 'Как путешествовать бесплатно',
+      'videoSubtitle' => 'и зарабатывать на этом',
+      'videoURL' => 'https://player.vimeo.com/video/402573812'
+  ]);
